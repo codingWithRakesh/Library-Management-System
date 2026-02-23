0a368cd7412b378792481e738d3f4b0b908ec22d
Tarapada Garai <146563644+codingWithRakesh@users.noreply.github.com>
2026-02-24 01:59:25 +0530

    Add HTML structure for book and issue management pages

--- admin/addBook/addBook.php
+++ admin/addBook/addBook.php
@@ -3,10 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Student Profile</title>
+    <title>Add Book Management System   </title>
 </head>
 <body>
-    <h2>Student Profile</h2>
-    
+    <h2>Add Book Management System</h2>
 </body>
 </html>
---
+++ admin/issue/issue.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Issue Book Management System</title>
+</head>
+<body>
+    <h2>Issue Book Management System</h2>
+</body>
+</html>
