4099d77231bf86e09711f4244da03ff8c227bd13
ReactwithAnand <anandparjapati21@gmail.com>
2026-02-27 09:14:45 +0530

    update

--- home/index.php
+++ home/index.php
@@ -50,6 +50,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Library Catalog</title>
+    <link rel="icon" href="../../assets/images/logo3.png" type="image/png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     
     <style>
--- search/search.php
+++ search/search.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="icon" href="../../assets/images/logo3.png" type="image/png">
 </head>
 <body>
     <h1>Search Results</h1>
